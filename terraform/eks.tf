1e0bcfad8b41b0ec627c7c123f6350a1615f3f84
cloudexpertsltd <cloudexpertsltd@gmail.com>
2026-03-23 21:06:40 +0600

    commit20

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -1,6 +1,6 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "19.21.0"   # pin exact version (important!)
+  version = "19.21.0"
 
   cluster_name    = var.cluster_name
   cluster_version = "1.29"
