14a4860a296a02a35826aed7762c8ecf58481620
cloudexpertsltd <cloudexpertsltd@gmail.com>
2026-03-23 16:46:07 +0600

    commit2

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,11 @@
 terraform {
   backend "s3" {
-    bucket         = "my-terraform-state-bucket"
+    bucket         = "cloudex-terraform-state-bucket"
     key            = "eks/terraform.tfstate"
     region         = "ap-southeast-1"
     dynamodb_table = "terraform-lock"
+
+    # Force path style to avoid region redirect issues
+    s3_force_path_style = true
   }
 }
