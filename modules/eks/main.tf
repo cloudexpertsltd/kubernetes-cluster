41d5b150dbc6e619b92bc53e50ba3b524a67ff5c
cloudexpertsltd <cloudexpertsltd@gmail.com>
2026-03-25 00:21:49 +0600

    commit109

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -1,27 +1,25 @@
-provider "aws" {
-  region = var.region
-}
-
 module "eks" {
-  source          = "terraform-aws-modules/eks/aws"
-  version         = "20.3.0"
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.3.0"
 
   cluster_name    = var.cluster_name
-  cluster_version = "1.35"
+  cluster_version = "1.27"
 
-  vpc_id            = var.vpc_id
-  subnet_ids        = var.public_subnet_ids
-  cluster_endpoint_private_access = true
-  cluster_endpoint_public_access  = true
-  cluster_endpoint_public_access_cidrs = ["0.0.0.0/0"]
- 
+  vpc_id = module.vpc.vpc_id
+  # This is the correct argument for public subnets
+  public_subnets = module.vpc.public_subnet_ids
+
+  # Let the module create roles automatically
+  create_eks_role  = true
+  create_node_groups_role = true
+
+  # Define a simple managed node group
   eks_managed_node_groups = {
     default = {
-      desired_capacity = var.desired_capacity
-      min_size         = var.min_capacity
-      max_size         = var.max_capacity
-      instance_types   = [var.instance_type]
-      # iam_role_arn   = var.node_group_role_arn # optional
+      desired_capacity = 2
+      min_size         = 1
+      max_size         = 3
+      instance_types   = ["t3.medium"]
     }
   }
 
--- modules/eks/outputs.tf
+++ modules/eks/outputs.tf
@@ -1,27 +1,15 @@
-output "cluster_id" {
-  value = module.eks.cluster_id
-}
-
-output "cluster_endpoint" {
-  value = module.eks.cluster_endpoint
-}
-
 output "cluster_name" {
-  description = "Name of the EKS cluster"
-  value       = aws_eks_cluster.this[0].name
+  value = aws_eks_cluster.this[0].name
 }
 
 output "cluster_arn" {
-  description = "ARN of the EKS cluster"
-  value       = aws_eks_cluster.this[0].arn
-}
-
-output "eks_managed_node_group_names" {
-  description = "Names of all managed node groups"
-  value       = [for ng in aws_eks_node_group.this : ng.node_group_name]
+  value = aws_eks_cluster.this[0].arn
 }
 
 output "eks_managed_node_group_arns" {
-  description = "ARNs of all managed node groups"
-  value       = [for ng in aws_eks_node_group.this : ng.arn]
+  value = [for ng in aws_eks_node_group.this : ng.arn]
+}
+
+output "eks_managed_node_group_names" {
+  value = [for ng in aws_eks_node_group.this : ng.node_group_name]
 }
