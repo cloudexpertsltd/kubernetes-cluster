43d7127970a96bb999f004dc911af0e4e5601151
cloudexpertsltd <cloudexpertsltd@gmail.com>
2026-03-25 00:48:13 +0600

    commit118

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -5,19 +5,17 @@ module "eks" {
   cluster_name    = var.cluster_name
   cluster_version = "1.35"
 
+  # Provide VPC and subnets
   vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
+  subnet_ids = module.vpc.public_subnet_ids
 
-  cluster_endpoint_private_access       = true
-  cluster_endpoint_public_access        = true
-  cluster_endpoint_public_access_cidrs = ["0.0.0.0/0"]
-
+  # Managed node groups (v20 syntax)
   eks_managed_node_groups = {
     default = {
-      desired_size   = 2
-      min_size       = 1
-      max_size       = 3
-      instance_types = ["t3.medium"]
+      desired_capacity = 2
+      min_size         = 1
+      max_size         = 3
+      instance_types   = ["t3.medium"]
     }
   }
 
