4e431083a0e7cfbd1f346a0e57e1e2c0b2d5636c
cloudexpertsltd <cloudexpertsltd@gmail.com>
2026-03-25 00:41:18 +0600

    commit115

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -5,10 +5,6 @@ module "eks" {
   cluster_name    = var.cluster_name
   cluster_version = "1.35"
 
-  # Let the module create IAM roles automatically
-  create_eks_role  = true
-  create_node_groups_role = true
-
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.public_subnet_ids
 
