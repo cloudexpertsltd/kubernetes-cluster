4b4531956a24483d966084d1ae7e9ce4ad4a2efb
cloudexpertsltd <cloudexpertsltd@gmail.com>
2026-03-25 01:40:30 +0600

    commit125

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -11,9 +11,7 @@ variable "public_subnet_cidrs" {
 }
 
 variable "region" {
-  description = "AWS region"
-  type        = string
-  default     = "ap-southeast-1"
+  type = string
 }
 
 variable "cluster_name" {
