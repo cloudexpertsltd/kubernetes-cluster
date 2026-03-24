fceeab6c8c5a43146e13ee0237d7e8bbc2f0ff5e
cloudexpertsltd <cloudexpertsltd@gmail.com>
2026-03-24 17:33:02 +0600

    commit26

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -6,7 +6,7 @@ module "eks" {
   version = "20.3.0"
 
   cluster_name    = var.cluster_name
-  cluster_version = "1.25"
+  cluster_version = "1.35"
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
