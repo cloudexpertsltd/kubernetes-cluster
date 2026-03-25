c06ea29a28b973805780fcfd9d55932c90d6b2a0
cloudexpertsltd <cloudexpertsltd@gmail.com>
2026-03-25 18:48:39 +0600

    commit22

--- argocd.tf
+++ argocd.tf
@@ -1,8 +1,12 @@
 resource "helm_release" "argocd" {
-  name       = "argocd"
-  namespace  = "argocd"
-  repository = "https://argoproj.github.io/argo-helm"
-  chart      = "argo-cd"
+  name             = "argocd"
+  namespace        = "argocd"
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argo-cd"
   create_namespace = true
-  depends_on = [module.eks] 
+
+  depends_on = [
+    module.eks,
+    kubernetes_config_map.aws_auth
+  ]
 }
