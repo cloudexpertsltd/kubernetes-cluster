74ec4d657f6a4c3e46f78239b207f81657a18b27
cloudexpertsltd <cloudexpertsltd@gmail.com>
2026-03-25 18:33:55 +0600

    commit19

--- aws-auth.tf
+++ aws-auth.tf
@@ -1,4 +1,7 @@
-# Get the EKS cluster info
+###############################
+# aws-auth.tf
+###############################
+
 data "aws_eks_cluster" "this" {
   name = module.eks.cluster_name
 }
@@ -7,14 +10,12 @@ data "aws_eks_cluster_auth" "this" {
   name = module.eks.cluster_name
 }
 
-# Kubernetes provider for accessing aws-auth ConfigMap
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.this.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.this.token
 }
 
-# aws-auth ConfigMap
 resource "kubernetes_config_map" "aws_auth" {
   metadata {
     name      = "aws-auth"
@@ -25,14 +26,14 @@ resource "kubernetes_config_map" "aws_auth" {
     mapRoles = yamlencode([
       # GitHub Actions runner role
       {
-        rolearn  = "arn:aws:iam::865809098262:role/GitHubRunnerRole"
+        rolearn  = "arn:aws:iam::865809098262:role/GitHubRunnerRole"  # <-- your runner role ARN
         username = "github"
         groups   = ["system:masters"]
       },
 
-      # Managed Node Group role
+      # EKS Node Group role (hardcoded)
       {
-        rolearn  = module.eks.managed_node_groups["default"].iam_role_arn
+        rolearn  = "arn:aws:iam::865809098262:role/default-eks-node-group-20260324201906423700000001"  # <-- replace with your node group IAM role ARN from AWS console
         username = "system:node:{{EC2PrivateDNSName}}"
         groups   = ["system:bootstrappers","system:nodes"]
       }
