7c2ff93ad225f4d7d395fe725b157ec48d202401
cloudexpertsltd <cloudexpertsltd@gmail.com>
2026-03-26 00:59:24 +0600

    commit8

--- modules/eks/outputs.tf
+++ modules/eks/outputs.tf
@@ -6,8 +6,6 @@ output "cluster_arn" {
   value = module.this_eks.cluster_arn
 }
 
-
-output "eks_managed_node_groups" {
-  description = "Managed node group resources."
-  value = aws_eks_node_group.this
+eks_managed_node_groups_iam_role_arns = {
+  <node_group_name> = <iam_role_arn>
 }
