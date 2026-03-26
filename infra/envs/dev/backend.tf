6114db8a779ac2c12d6ab53b5deaafcdda8e3385
cloudexpertsltd <cloudexpertsltd@gmail.com>
2026-03-26 23:21:32 +0600

    commi31

--- infra/envs/dev/backend.tf
+++ infra/envs/dev/backend.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = "~> 1.5.7"
 
   backend "s3" {
-    bucket         = "cloudex-terraform-state-bucket "
+    bucket         = "cloudex-terraform-state-bucket"
     key            = "eks/dev/terraform.tfstate"  # temporarily hardcoded
     region         = "ap-southeast-1"
     dynamodb_table = "terraform-locks"
