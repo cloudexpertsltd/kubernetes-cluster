6bccd74165e1f2fdf4163ec11459a6242b2dfc67
cloudexpertsltd <cloudexpertsltd@gmail.com>
2026-03-27 17:25:32 +0600

    commit38

--- infra/envs/dev/outputs.tf
+++ infra/envs/dev/outputs.tf
@@ -1,3 +1,7 @@
+output "cluster_name" {
+  value = module.eks.cluster_name
+}
+
 output "vpc_id" {
   value = module.vpc.vpc_id
 }
@@ -5,8 +9,4 @@ output "vpc_id" {
 
 output "public_subnet_ids" {
   value = module.vpc.public_subnet_ids
-}
-
-output "cluster_name" {
-  value = module.eks.cluster_name
 }
