bd0e29b09a2b17540418b47b63aa6d3f7d255764
cloudexpertsltd <cloudexpertsltd@gmail.com>
2026-03-27 19:45:31 +0600

    commit61

--- platform/envs/dev/argocd.tf
+++ platform/envs/dev/argocd.tf
@@ -1,40 +1,9 @@
-# Fetch remote state of EKS cluster
-data "terraform_remote_state" "eks" {
-  backend = "s3"
-  config = {
-    bucket = "cloudex-terraform-state-bucket"
-    key    = "eks/dev/terraform.tfstate"  # path to your EKS state
-    region = "ap-southeast-1"
-  }
-}
-
-# AWS provider
-provider "aws" {
-  region = "ap-southeast-1"
-}
-
-# Local for cluster name
-locals {
-  cluster_name = data.terraform_remote_state.eks.outputs.cluster_name
-}
-
-# EKS data sources
-data "aws_eks_cluster" "eks" {
-  name = local.cluster_name
-}
-
-data "aws_eks_cluster_auth" "eks" {
-  name = local.cluster_name
-}
-
-# Kubernetes provider
+# Kubernetes provider using existing kubeconfig
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.eks.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.eks.token
+  config_path = "~/.kube/config"
 }
 
-# Helm provider uses Kubernetes provider automatically
+# Helm provider will automatically use the Kubernetes provider
 provider "helm" {}
 
 # Helm release for ArgoCD
