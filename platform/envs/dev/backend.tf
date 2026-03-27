31b7af476396f7992937df0aa3c5ed4ad37c40e1
cloudexpertsltd <cloudexpertsltd@gmail.com>
2026-03-27 13:54:52 +0600

    commit21

--- platform/envs/dev/backend.tf
+++ platform/envs/dev/backend.tf
@@ -1,8 +1,9 @@
-# platform/backend.tf
 terraform {
   backend "s3" {
-    bucket = "my-terraform-states"
-    key    = "platform/dev/terraform.tfstate"
-    region = "us-east-1"
+    bucket         = "cloudex-terraform-state-bucket"
+    key            = "platform/dev/argocd/terraform.tfstate"
+    region         = "ap-southeast-1"
+    dynamodb_table = "terraform-lock"
+    encrypt        = true
   }
 }
